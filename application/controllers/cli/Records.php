8f52447093a970207b97b5afe198dcc180946d3b
Dafydd Vaughan <dai@daibach.co.uk>
2016-08-03 10:55:08 +0100

    Store DMARC settings when querying record

--- application/controllers/cli/Records.php
+++ application/controllers/cli/Records.php
@@ -30,13 +30,30 @@ class Records extends CI_Controller {
 
     //SPF lookup
     $spf_record = $this->_do_txt_lookup($domain->domain_full, 'v=spf1');
-    $dmarc_record = $this->_do_txt_lookup($domain->domain_full, 'v=dmarc1');
+    $dmarc_record = $this->_do_dmarc_lookup($domain->domain_full);
 
     $this->records->update_domain($domain->domain_full, $spf_record, $dmarc_record);
 
   }
 
-  private function _do_txt_lookup($domain, $type) {
+  private function _do_dmarc_lookup($domain) {
+    $result = $this->_do_txt_lookup($domain, 'v=dmarc1', true);
+    if($result) {
+      if(strpos($result,'p=reject') !== false) {
+        return "reject";
+      } elseif(strpos($result,'p=quarantine') !== false) {
+        return "quarantine";
+      } elseif(strpos($result,'p=none') !== false) {
+        return "none";
+      } else {
+        return "other";
+      }
+    } else {
+      return "empty";
+    }
+  }
+
+  private function _do_txt_lookup($domain, $type, $return_result=false) {
 
     $dns_to_query = $domain;
     if($type === 'v=dmarc1') {
@@ -47,14 +64,20 @@ class Records extends CI_Controller {
 
     if($dns_result) {
       $record_found = false;
+      $record_contents = "";
       foreach($dns_result as $result) {
         if(strpos(strtolower($result->data),$type) !== false) {
           $this->records->create_record($domain, $result->data, $type);
           $record_found = true;
+          $record_contents = $result->data;
         }
       }
       if($record_found) {
-        return true;
+        if($return_result) {
+          return $record_contents;
+        } else {
+          return true;
+        }
       } else {
         $this->records->create_record($domain, NULL, $type);
         return false;
